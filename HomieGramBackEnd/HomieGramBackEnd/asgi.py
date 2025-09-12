643f3f9ae54f3b947c559f801d166bfbae7346fb
Fred <fredjaoko123@gmail.com>
2025-09-12 11:01:27 +0300

    Minor changes for deployment

--- HomieGramBackEnd/HomieGramBackEnd/asgi.py
+++ HomieGramBackEnd/HomieGramBackEnd/asgi.py
@@ -6,18 +6,21 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
+import django
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HomieGramBackEnd.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_project.settings")
 
-
+# Set up Django before importing anything dependent on it
+django.setup()
 
 print("ASGI is running")
+
+# Now it's safe to import chat.routing
+import chat.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
