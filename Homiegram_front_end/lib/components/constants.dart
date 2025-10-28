2a78fbdec4e8d50ac71a12300773cf03e3a5f2c3
masiDaniel <masidaniel02@gmail.com>
2025-10-28 23:46:19 +0300

    style(code):  removed unnescesarry spaces and lines

--- Homiegram_front_end/lib/components/constants.dart
+++ Homiegram_front_end/lib/components/constants.dart
@@ -2,8 +2,6 @@ class AppConstants {
   static const String baseUrl = 'https://api.jabbaliinovations.co.ke';
   static const String chatBaseUrl = 'wss://api.jabbaliinovations.co.ke';
 }
-// ;
-
 
 //  static const String baseUrl = 'http://192.168.100.38:8000';
 //   static const String chatBaseUrl = 'ws://192.168.100.38:8000';
